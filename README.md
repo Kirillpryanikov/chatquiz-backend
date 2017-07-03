82c7ac564d1704a0159719dd3410e906d873f441
Dimzdey <dimzdey@gmail.com>
2017-07-03 12:54:07 +0300

    Added readme

---
+++ README.md
@@ -0,0 +1,27 @@
+#ChatQuiz app
+
+---
+
+For local development:
+
+` node index.js `
+
+For staging:
+
+` npm run start:pm2:dev`
+
+For production:
+
+` npm run start:pm2:prod`
+
+To stop the server:
+
+`npm run stop:pm2`
+
+To restart server:
+
+`npm run restart:pm2`
+
+To show status:
+
+`npm run status:pm2`
--- config/config.js
+++ config/config.js
@@ -3,3 +3,11 @@ module.exports = {
   secret : 'rghehryynv8yofq384y809tpyuowEWY24397',
   appkey: '1234567890'
 };
+
+var env = process.env.NODE_ENV || 'development';
+console.log(env + ' ENV');
+if (env === 'development') {
+    process.env.PORT = 8080;
+} else if (env === 'production') {
+    process.env.PORT = 8080;
+}
